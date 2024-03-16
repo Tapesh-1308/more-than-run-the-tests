410736f3797d3be3cd7f1b2c8fb9fee4e697cd39
Tapesh <tapesh.dua420@gmail.com>
2024-03-16 09:41:35 +0530

    updated proxy url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'https://more-than-run-the-tests-api.onrender.com',
     },
   },
   plugins: [react()],
